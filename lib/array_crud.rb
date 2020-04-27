b8854e9ade9afefa7650ade7b1a573fb9d853386
Stephen Peters <7speter@gmail.com>
2020-04-27 15:30:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  starships = ["Enterprise", "Voyager", "Defiant", "Excelsior"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  starships << "Akira"
 end
 
 def add_element_to_start_of_array(array, element)
